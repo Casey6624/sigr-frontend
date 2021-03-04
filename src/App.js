1f221d92c064f685934ebdc0c418501a7ba9d79d
Casey <Casey@vouch.co.uk>
2021-03-04 11:19:48 +0000

    inital

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import * as signalR from "@microsoft/signalr";
+import "./App.css";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [hubConnection, setHubConnection] = useState();
+
+  useEffect(() => {
+    const conn = new signalR.HubConnectionBuilder()
+      .withUrl("http://localhost:7071/api")
+      .configureLogging(signalR.LogLevel.Information)
+      .build();
+
+    // Starts the SignalR connection
+    conn.start().then((a) => {
+      // Once started, invokes the sendConnectionId in our ChatHub inside our ASP.NET Core application.
+      if (conn.connectionId) {
+        conn.invoke("negotiate", conn.connectionId, {
+          customerId: "vch_customer_id",
+          personId: "vch_person_id",
+        });
+      }
+    });
+    setHubConnection(conn);
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>sigr</h1>
     </div>
   );
 }
